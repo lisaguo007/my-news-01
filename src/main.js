4115f829254997640211b6d52571ea03cb70fc56
lisaguo007 <18817680351@163.com>
2020-08-26 18:06:21 +0800

    更新vant-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+import { Button } from 'vant'
+
+// 导入通用样式
+import './styles/base.less'
+import './styles/iconfont.css'
+
+// Vue.use(Vant)
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
---
+++ src/views/Register.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>这是注册组件</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
